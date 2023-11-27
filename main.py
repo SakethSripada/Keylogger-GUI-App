2f39a3abe335e2c4b1773e64eb9ba4cd1a13ac03
Saketh Sripada <sakeats@gmail.com>
2023-11-26 18:10:04 -0600

    added search functionality, added menubar options, changed colors

--- main.py
+++ main.py
@@ -12,29 +12,47 @@ class KeyRecorder:
         self.style = ttk.Style()
         self.style.theme_use("clam")
 
-        self.root.configure(bg="#53A7C4")
+        self.root.configure(bg="white")
 
         self.menubar = tk.Menu(self.root)
 
         self.filemenu = tk.Menu(self.menubar, tearoff=0)
-        self.filemenu.add_command(label="Exit?", command=exit)
+        self.filemenu.add_command(label="Exit", command=self.close_app)
         self.filemenu.add_separator()
+        self.filemenu.add_command(label="Save to File", command=self.write_to_file)
 
         self.actionmenu = tk.Menu(self.menubar, tearoff=0)
+        self.actionmenu.add_command(label="Start Recording", command=self.record_keystrokes)
+        self.actionmenu.add_separator()
+        self.actionmenu.add_command(label="Clear", command=self.clear_recording)
+        self.actionmenu.add_separator()
+        self.actionmenu.add_command(label="Stop Recording", command=self.quit_record)
 
         self.menubar.add_cascade(menu=self.filemenu, label="File")
         self.menubar.add_cascade(menu=self.actionmenu, label="Actions")
 
         self.root.config(menu=self.menubar)
 
-        self.label = tk.Label(self.root, text="Keystroke Recorder", font=("Arial", 18), background="#53A7C4")
+        self.label = tk.Label(self.root, text="Keystroke Recorder", font=("Arial", 18), background="white")
         self.label.pack(padx=10, pady=10)
 
-        self.status_label = tk.Label(self.root, text="Recording Inactive", font=("Arial", 18), background="#53A7C4")
+        self.status_label = tk.Label(self.root, text="Recording Inactive", font=("Arial", 18), background="white",
+                                     foreground="red")
         self.status_label.pack(side=tk.BOTTOM, fill=tk.X)
 
         self.textbox = tk.Text(self.root, height=5, font=("Arial", 16), foreground="white", background="black")
         self.textbox.pack(padx=10, pady=10)
+
+        search_frame = ttk.Frame(self.root)
+        search_frame.pack(fill=tk.X)
+
+        self.search_entry = ttk.Entry(search_frame, background="white")
+        self.search_entry.pack(side=tk.LEFT, padx=1, pady=1, fill=tk.X, expand=True)
+
+        self.search_button = ttk.Button(search_frame, text="Search", command=self.search_recording)
+        self.search_button.pack(padx=10, pady=10)
+
+        ttk.Frame(self.root, height=10, relief='flat', borderwidth=0).pack()
 
         buttonframe = ttk.Frame(self.root)
         buttonframe.pack()
@@ -70,6 +88,7 @@ class KeyRecorder:
             self.listener.start()
             self.recording_active = True
             self.update_status("Recording Active")
+            self.status_label.config(foreground="green")
 
     def on_key_press(self, key):
         if isinstance(key, keyboard.Key):
@@ -107,8 +126,23 @@ class KeyRecorder:
     def quit_record(self):
         if self.recording_active:
             self.update_status("Recording Inactive")
+            self.status_label.config(foreground="red")
             self.listener.stop()
             self.recording_active = False
+
+    def search_recording(self):
+        query = self.search_entry.get()
+        if query:
+            search_start = "1.0"
+            while True:
+                search_start = self.textbox.search(query, search_start, stopindex=tk.END)
+                if not search_start:
+                    break
+                search_end = f"{search_start}+{len(query)}c"
+                self.textbox.tag_add("search", search_start, search_end)
+                search_start = search_end
+            self.textbox.tag_config("search", background="yellow", foreground="black")
+            self.textbox.see(search_end)
 
 
 KeyRecorder()
